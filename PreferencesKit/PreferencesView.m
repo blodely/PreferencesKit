940d4766b6786e51a578d7ed9b30357cbfc62676
Luo Yu <indie.luo@gmail.com>
2016-07-21 11:58:04 +0800

    Add : preferences view

---
+++ PreferencesKit/PreferencesView.m
@@ -0,0 +1,21 @@
+//
+//  PreferencesView.m
+//  PreferencesSample
+//
+//  CREATED BY LUO YU ON 2016-07-21.
+//  COPYRIGHT © 2016 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import "PreferencesView.h"
+
+@implementation PreferencesView
+
+/*
+// ONLY OVERRIDE drawRect: IF YOU PERFORM CUSTOM DRAWING.
+// AN EMPTY IMPLEMENTATION ADVERSELY AFFECTS PERFORMANCE DURING ANIMATION.
+- (void)drawRect:(CGRect)rect {
+	// DRAWING CODE
+}
+*/
+
+@end
