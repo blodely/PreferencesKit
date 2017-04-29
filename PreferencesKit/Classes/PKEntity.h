503983c86dba9398a69dbad215f5358cd3ced15b
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-04-29 14:15:10 +0800

    [Modify] entity define

--- PreferencesKit/Classes/PKEntity.h
+++ PreferencesKit/Classes/PKEntity.h
@@ -8,25 +8,73 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum {
+/**
+ define preferencesKit entity type
+
+ - PKEntityTypeNone: default type none
+ - PKEntityTypeBoolean: switch type
+ - PKEntityTypeSectionNumber: forgot
+ - PKEntityTypeNumberInt: int number
+ - PKEntityTypeNumberDouble: double number
+ - PKEntityTypeString: string
+ */
+typedef NS_ENUM(NSInteger,  PKEntityType) {
+	PKEntityTypeNone = 0,
 	PKEntityTypeBoolean = 1,
 	PKEntityTypeSectionNumber = 2,
 	PKEntityTypeNumberInt = 3,
 	PKEntityTypeNumberDouble = 4,
 	PKEntityTypeString = 11,
-} PKEntityType;
+};
 
 @interface PKEntity : NSObject <NSCoding, NSCopying>
 
+/**
+ initial method
+
+ @param atype entity type
+ @return instance
+ */
 - (instancetype)initWithType:(PKEntityType)atype;
 
+/**
+ entity name
+ */
 @property (nonatomic, strong) NSString *name;
+
+/**
+ entity displayed title
+ */
 @property (nonatomic, strong) NSString *title;
+
+/**
+ entity displayed subtitle
+ */
 @property (nonatomic, strong) NSString *subtitle;
+
+/**
+ entity type, read only, setted by creation
+ */
 @property (nonatomic, readonly) PKEntityType type;
+
+/**
+ entity object value, id
+ */
 @property (nonatomic) id value;
+
+/**
+ entity boolean value
+ */
 @property (nonatomic, assign) BOOL valueBool;
+
+/**
+ entity integer value
+ */
 @property (nonatomic, assign) NSInteger valueInt;
+
+/**
+ entity double value
+ */
 @property (nonatomic, assign) double valueDouble;
 
 @end
