c043ee28668c2cf556f4c15b3643c66012c32170
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-05-10 11:35:14 +0800

    [Remove] pods folder from vcs

--- Example/Pods/Target Support Files/Pods-PreferencesKit_Tests/Pods-PreferencesKit_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PreferencesKit_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PreferencesKit_Tests
-@end
--- Example/Pods/Target Support Files/PreferencesKit/PreferencesKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PreferencesKit : NSObject
-@end
-@implementation PodsDummy_PreferencesKit
-@end
